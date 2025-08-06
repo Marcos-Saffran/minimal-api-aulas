2ad5687d91b614a50b4a67ac5728a995d9d2f8d1
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2025-08-06 11:49:45 -0300

    feature: Add seeding for Administrador entity with initial data, aula: criando Seed para cadastrar administrador padrão

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -15,6 +15,19 @@ public class DbContexto : DbContext
 
     public DbSet<Administrador> Administradores { get; set; } = default!;
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrador>().HasData(
+            new Administrador
+            {
+                Id = 1,
+                Email = "adm@teste.com",
+                Senha = "123456",
+                Perfil = "Adm"
+            }
+        );
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (optionsBuilder.IsConfigured)
